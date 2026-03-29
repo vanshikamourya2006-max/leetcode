7fd01e1713a165b472826ba389d34a652d2aeaac
Vanshika Mourya <vanshikamourya2006@gmail.com>
2026-03-29 17:49:18 +0530

    Time: 2 ms (90.70%) | Memory: 48.1 MB (13.89%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        int zero=0;
         int move=0;
         for(int i=0;i<nums.length;i++){
            if(nums[i]!=0){
@@ -14,7 +13,6 @@ class Solution {
             nums[move]=0;
             move++;
         }
-        System.out.println(Arrays.toString(nums));
         
     }
 }
