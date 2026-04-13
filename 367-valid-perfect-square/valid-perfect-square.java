4503a18a621414014c0e36aaa0ef5a9be907ec7d
Vanshika Mourya <vanshikamourya2006@gmail.com>
2026-04-13 23:52:22 +0530

    Time: 1 ms (20.82%) | Memory: 42.1 MB (44.78%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+       int odd=1;
+       while(num>0) {
+            num=num-odd;
+            odd=odd+2;
+       }
+       return num==0;
+       
+    }
+}
