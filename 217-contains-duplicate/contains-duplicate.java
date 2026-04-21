4f60e141deacfe9a2e7a4a93a4d07623f2393600
Vanshika Mourya <vanshikamourya2006@gmail.com>
2026-04-21 14:12:29 +0530

    Time: 15 ms (64.66%) | Memory: 93.5 MB (50.23%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hashset= new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(hashset.contains(nums[i])){
+                return true;
+            }
+            hashset.add(nums[i]);
+        } 
+        return false;
+    }
+}
