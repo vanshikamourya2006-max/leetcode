b49a85d03141632966110cd5ebfcc4c5dc6b51c4
Vanshika Mourya <vanshikamourya2006@gmail.com>
2026-06-18 10:46:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (96.37%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(n==3){
+            return 3;
+        }
+        int a=1,b=2,c;
+        for(int i=3;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+        return b;
+    }
+}
